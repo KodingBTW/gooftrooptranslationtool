4febeb695e4afe534e5534e449c7542bd04d87ea
Rodrigo Muñoz <83840135+KodingBTW@users.noreply.github.com>
2024-11-24 22:23:23 -0300

    Update insert.bat

--- insert.bat
+++ insert.bat
@@ -5,7 +5,7 @@ set tblFile="encoderG.tbl"
 set pointersStartAddress=0x5E81D
 set textStartAddress=0x5E881
 set headerSize=0x50000
-set textSize=111E
+set textSize=0x111E
 :loop
 	pause
 	HexString -e %textFile% %textStartAddress% %textSize% %pointersStartAddress% %headerSize% %romName% %tblFile%
